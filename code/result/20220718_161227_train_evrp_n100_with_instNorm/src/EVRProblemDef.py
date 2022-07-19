7b049a86a8c79d7843902bb9a021621c7d1f62ad
alwaysbyx <496127893@qq.com>
2022-07-18 17:06:23 -0700

    add

---
+++ code/result/20220718_161227_train_evrp_n100_with_instNorm/src/EVRProblemDef.py
@@ -0,0 +1,51 @@
+
+import torch
+import numpy as np
+from scipy.spatial import distance_matrix
+
+
+def get_random_problems(batch_size, problem_size):
+    depot_xy = torch.rand(size=(batch_size, 1, 2))
+    # shape: (batch, 1, 2)
+    node_xy = torch.rand(size=(batch_size, problem_size, 2))
+    # shape: (batch, problem, 2)
+    if problem_size == 20:
+        scaler = 3
+    elif problem_size == 50:
+        scaler = 4
+    elif problem_size == 100:
+        scaler = 5
+    else:
+        raise NotImplementedError
+    total_n = 1+problem_size
+    dm = np.zeros((batch_size, total_n, total_n),dtype=np.float32)
+    for b in range(batch_size):
+        depot = depot_xy[b].numpy()
+        node = node_xy[b].numpy()
+        nodes = np.concatenate([depot, node], axis=0)
+        distance = distance_matrix(nodes,nodes) / scaler
+        dm[b] = distance 
+    # shape: (batch, Problem)
+    # depot_xy, node_xy, distance_to_depot, distance_matrix
+    return depot_xy, node_xy, torch.from_numpy(dm[:,0,:]), torch.from_numpy(dm)
+
+
+def augment_xy_data_by_8_fold(xy_data):
+    # xy_data.shape: (batch, N, 2)
+    x = xy_data[:, :, [0]]
+    y = xy_data[:, :, [1]]
+    # x,y shape: (batch, N, 1)
+
+    dat1 = torch.cat((x, y), dim=2)
+    dat2 = torch.cat((1 - x, y), dim=2)
+    dat3 = torch.cat((x, 1 - y), dim=2)
+    dat4 = torch.cat((1 - x, 1 - y), dim=2)
+    dat5 = torch.cat((y, x), dim=2)
+    dat6 = torch.cat((1 - y, x), dim=2)
+    dat7 = torch.cat((y, 1 - x), dim=2)
+    dat8 = torch.cat((1 - y, 1 - x), dim=2)
+
+    aug_xy_data = torch.cat((dat1, dat2, dat3, dat4, dat5, dat6, dat7, dat8), dim=0)
+    # shape: (8*batch, N, 2)
+
+    return aug_xy_data
---
+++ code/result/20220718_161727_train_evrp_n100_with_instNorm/src/environment.py
@@ -0,0 +1,259 @@
+
+from dataclasses import dataclass
+import torch
+
+from EVRProblemDef import get_random_problems, augment_xy_data_by_8_fold
+
+
+@dataclass
+class Reset_State:
+    depot_xy: torch.Tensor = None
+    # shape: (batch, 1, 2)
+    node_xy: torch.Tensor = None
+    # shape: (batch, problem, 2)
+    node_distance: torch.Tensor = None
+    # shape: (batch, problem)
+    dm: torch.Tensor = None
+    # shape: (batch, problem+1, problem+1)
+
+@dataclass
+class Step_State:
+    BATCH_IDX: torch.Tensor = None
+    POMO_IDX: torch.Tensor = None
+    # shape: (batch, pomo)
+    selected_count: int = None
+    battery: torch.Tensor = None
+    # shape: (batch, pomo)
+    current_node: torch.Tensor = None
+    # shape: (batch, pomo)
+    ninf_mask: torch.Tensor = None
+    # shape: (batch, pomo, problem+1)
+    finished: torch.Tensor = None
+    # shape: (batch, pomo)
+
+
+class EVRPEnv:
+    def __init__(self, **env_params):
+        # Const @INIT
+        ####################################
+        self.env_params = env_params
+        self.problem_size = env_params['problem_size']
+        self.pomo_size = env_params['pomo_size']
+
+        self.FLAG__use_saved_problems = False
+        self.saved_depot_xy = None
+        self.saved_node_xy = None
+        self.saved_node_distance = None
+        self.saved_dm = None # distance matrix
+        self.saved_index = None
+
+        # Const @Load_Problem
+        ####################################
+        self.batch_size = None
+        self.BATCH_IDX = None
+        self.POMO_IDX = None
+        # IDX.shape: (batch, pomo)
+        self.depot_node_xy = None
+        # shape: (batch, problem+1, 2)
+        self.depot_node_dm = None
+        # shape: (batch, problem+1, problem+1)
+
+        # Dynamic-1
+        ####################################
+        self.selected_count = None
+        self.current_node = None
+        # shape: (batch, pomo)
+        self.selected_node_list = None
+        # shape: (batch, pomo, 0~)
+
+        # Dynamic-2
+        ####################################
+        self.at_the_depot = None
+        # shape: (batch, pomo)
+        self.battery = None
+        # shape: (batch, pomo)
+        self.visited_ninf_flag = None
+        # shape: (batch, pomo, problem+1)
+        self.ninf_mask = None
+        # shape: (batch, pomo, problem+1)
+        self.finished = None
+        # shape: (batch, pomo)
+
+        # states to return
+        ####################################
+        self.reset_state = Reset_State()
+        self.step_state = Step_State()
+
+    def use_saved_problems(self, filename, device):
+        self.FLAG__use_saved_problems = True
+
+        loaded_dict = torch.load(filename, map_location=device)
+        self.saved_depot_xy = loaded_dict['depot_xy']
+        self.saved_node_xy = loaded_dict['node_xy']
+        self.saved_node_distance = loaded_dict['node_distance']
+        self.saved_dm = loaded_dict['dm']
+        self.saved_index = 0
+
+    def load_problems(self, batch_size, aug_factor=1):
+        self.batch_size = batch_size
+
+        if not self.FLAG__use_saved_problems:
+            depot_xy, node_xy,  node_distance, distance_matrix = get_random_problems(batch_size, self.problem_size)
+        else:
+            depot_xy = self.saved_depot_xy[self.saved_index:self.saved_index+batch_size]
+            node_xy = self.saved_node_xy[self.saved_index:self.saved_index+batch_size]
+            node_distance = self.saved_node_distance[self.saved_index:self.saved_index+batch_size]
+            self.saved_index += batch_size
+
+        if aug_factor > 1:
+            if aug_factor == 8:
+                self.batch_size = self.batch_size * 8
+                depot_xy = augment_xy_data_by_8_fold(depot_xy)
+                node_xy = augment_xy_data_by_8_fold(node_xy)
+                node_distance = node_distance.repeat(8, 1)
+                distance_matrix = distance_matrix.repeat(8,1)
+            else:
+                raise NotImplementedError
+
+        self.depot_node_xy = torch.cat((depot_xy, node_xy), dim=1)
+        # shape: (batch, problem+1, 2)
+        self.depot_node_dm = distance_matrix
+        # shape: (batch, problem+1, problem+1)
+        depot_distance = torch.zeros(size=(self.batch_size, 1))
+        # shape: (batch, 1)
+        self.depot_node_distance = torch.cat((depot_distance, node_distance), dim=1)
+        # shape: (batch, problem+1)
+        self.node_distance = node_distance
+        # shape: (batch, problem)
+        
+
+        self.BATCH_IDX = torch.arange(self.batch_size)[:, None].expand(self.batch_size, self.pomo_size)
+        self.POMO_IDX = torch.arange(self.pomo_size)[None, :].expand(self.batch_size, self.pomo_size)
+
+        self.reset_state.depot_xy = depot_xy
+        self.reset_state.node_xy = node_xy
+        self.reset_state.node_distance = node_distance
+        self.reset_state.dm = distance_matrix
+
+        self.step_state.BATCH_IDX = self.BATCH_IDX
+        self.step_state.POMO_IDX = self.POMO_IDX
+
+    def reset(self):
+        self.selected_count = 0
+        self.current_node = None
+        # shape: (batch, pomo)
+        self.selected_node_list = torch.zeros((self.batch_size, self.pomo_size, 0), dtype=torch.long)
+        # shape: (batch, pomo, 0~)
+        self.at_the_depot = torch.ones(size=(self.batch_size, self.pomo_size), dtype=torch.bool)
+        # shape: (batch, pomo)
+        self.battery = torch.ones(size=(self.batch_size, self.pomo_size))
+        # shape: (batch, pomo)
+        self.visited_ninf_flag = torch.zeros(size=(self.batch_size, self.pomo_size, self.problem_size+1))
+        # shape: (batch, pomo, problem+1)
+        self.ninf_mask = torch.zeros(size=(self.batch_size, self.pomo_size, self.problem_size+1))
+        # shape: (batch, pomo, problem+1)
+        self.finished = torch.zeros(size=(self.batch_size, self.pomo_size), dtype=torch.bool)
+        # shape: (batch, pomo)
+
+        reward = None
+        done = False
+        return self.reset_state, reward, done
+
+    def pre_step(self):
+        self.step_state.selected_count = self.selected_count
+        self.step_state.battery = self.battery
+        self.step_state.current_node = self.current_node
+        self.step_state.ninf_mask = self.ninf_mask
+        self.step_state.finished = self.finished
+
+        reward = None
+        done = False
+        return self.step_state, reward, done
+
+    def step(self, selected):
+        # selected.shape: (batch, pomo)
+
+        # Dynamic-1 selected node 
+        ####################################
+        self.selected_count += 1
+        if self.current_node == None:
+            self.pre_node = selected
+        else:
+            self.pre_node = self.current_node
+        self.current_node = selected
+        # shape: (batch, pomo)
+        self.selected_node_list = torch.cat((self.selected_node_list, self.current_node[:, :, None]), dim=2)
+        # shape: (batch, pomo, 0~)
+
+        # Dynamic-2 battery loss
+        ####################################
+        self.at_the_depot = (selected == 0) # shape: (batch, pomo)
+
+        pre_index = self.pre_node[:,:,None]
+        distance_matrix =  self.depot_node_dm[:, None, :, :].expand(self.batch_size, self.pomo_size, self.problem_size+1, self.problem_size+1)
+        # shape: (batch, pomo, problem+1, problem+1)
+        curr_index = selected[:, :, None]
+        distance_list = []
+        for i in range(self.problem_size+1):
+            distance_list.append(distance_matrix[:,:,:,i].gather(dim=2,index=curr_index).squeeze()) # [current node, [:]]
+        distance_list = torch.stack(distance_list,dim=2)
+        # shape: (batch, pomo, problem+1)
+        battery_loss = distance_list.gather(dim=2, index=pre_index).squeeze(dim=2) # [current node : pre node]
+        # shape: (batch, pomo)
+        self.battery -= battery_loss
+        self.battery[self.at_the_depot] = 1 # full charge at the depot
+
+        self.visited_ninf_flag[self.BATCH_IDX, self.POMO_IDX, selected] = float('-inf') # selected node not visit next time
+        # shape: (batch, pomo, problem+1)
+        self.visited_ninf_flag[:, :, 0][~self.at_the_depot] = 0  # depot is considered unvisited, unless you are AT the depot
+
+        self.ninf_mask = self.visited_ninf_flag.clone()
+        round_error_epsilon = 0.00001
+        depot_node_distance = self.depot_node_distance[:,None,:].expand(self.batch_size, self.pomo_size, self.problem_size+1) # every node to depot
+        # shape: (batch, pomo, problem+1)
+        # shape: (batch, pomo, problem+1)
+        distance_too_large = depot_node_distance + distance_list + round_error_epsilon > self.battery[:, :, None]
+        # shape: (batch, pomo, problem+1)
+        self.ninf_mask[distance_too_large] = float('-inf') # battery safe constraint
+        # shape: (batch, pomo, problem+1)
+
+        newly_finished = (self.visited_ninf_flag == float('-inf')).all(dim=2)
+        # shape: (batch, pomo)
+        self.finished = self.finished + newly_finished
+        # shape: (batch, pomo)
+
+        # do not mask depot for finished episode.
+        self.ninf_mask[:, :, 0][self.finished] = 0
+
+        self.step_state.selected_count = self.selected_count
+        self.step_state.load = self.load
+        self.step_state.current_node = self.current_node
+        self.step_state.ninf_mask = self.ninf_mask
+        self.step_state.finished = self.finished
+
+        # returning values
+        done = self.finished.all()
+        if done:
+            reward = -self._get_travel_distance()  # note the minus sign!
+        else:
+            reward = None
+
+        return self.step_state, reward, done
+
+    def _get_travel_distance(self):
+        gathering_index = self.selected_node_list[:, :, :, None].expand(-1, -1, -1, 2)
+        # shape: (batch, pomo, selected_list_length, 2)
+        all_xy = self.depot_node_xy[:, None, :, :].expand(-1, self.pomo_size, -1, -1)
+        # shape: (batch, pomo, problem+1, 2)
+
+        ordered_seq = all_xy.gather(dim=2, index=gathering_index)
+        # shape: (batch, pomo, selected_list_length, 2)
+
+        rolled_seq = ordered_seq.roll(dims=2, shifts=-1)
+        segment_lengths = ((ordered_seq-rolled_seq)**2).sum(3).sqrt()
+        # shape: (batch, pomo, selected_list_length)
+
+        travel_distances = segment_lengths.sum(2)
+        # shape: (batch, pomo)
+        return travel_distances
+
